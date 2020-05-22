bbc4c1ce84ae9a27affec22eb20e2e780faae178
justin-sumida <sum18004@byui.edu>
2020-05-22 11:37:56 -0700

    inital add of project

--- db/mDB.sql
+++ db/mDB.sql
@@ -16,3 +16,14 @@ CREATE TABLE last50Contacts(
     contact_id INT NOT NULL REFERENCES contact(id),
     description TEXT NOT NULL
 );
+
+INSERT INTO userReview(review, contentReview, name) VALUES ('5', 'Kirk did a fantastic job on our lights, professional and on time. Left it clean too.', 'Jane Doe');
+INSERT INTO userReview(review, contentReview, name) VALUES ('5', 'Kirk came early to the job, got out within 3 hours, and did a great job!', 'John Doe');
+INSERT INTO userReview(review, contentReview, name) VALUES ('4.5', 'Very professional guy, one of the best in the business.', 'Jilly Joel');
+INSERT INTO userReview(review, contentReview, name) VALUES ('5', 'Left our house cleaner than it was! Great.', 'LaFonda');
+
+INSERT INTO contact(name, email, contact) VALUES('Billy Bob', 'bill@gmail.com', 'Hi, I was wondering how much it would be to...');
+INSERT INTO contact(name, email, contact) VALUES('Jilly Jane', 'jill@gmail.com', 'How much would you charge to install 8 kitchen lights?');
+INSERT INTO contact(name, email, contact) VALUES('Justin Sumida', 'sum18004@byui.edu' , 'Do you install QuietCool still?');
+
+
---
+++ prove07.php
@@ -0,0 +1,32 @@
+<?php
+    require 'ta051.php';
+    $db = get_db();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The Recessed Light Guy</title>
+</head>
+<body>
+    <h1>The Recessed Light Guy</h1>
+    <hr><br>
+    <h2>Reviews:</h2><br>
+    <p> <?php
+       $statement = $db->prepare("SELECT review, contentReview, name FROM userReview");
+       $statement->execute();
+      
+       while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+       {
+           $review = $row['review'];
+           $content = $row['contentReview'];
+           $name = $row['name'];
+       
+           echo "<p><strong>$name $review</strong><br>$content<p>";
+       }
+       
+        ?>
+    </p>
+</body>
+</html>
