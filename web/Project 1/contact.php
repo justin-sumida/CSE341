6760e3e2a26a5431e302ccfe3ba21f24b26d37f8
justin-sumida <sum18004@byui.edu>
2020-06-04 18:36:19 -0700

    Filling text for contact

--- web/Project 1/contact.php
+++ web/Project 1/contact.php
@@ -5,6 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel ="stylesheet" href="rlg.css">
     <title>Contact Us</title>
+    <style>
+        .row {
+          display: flex;
+          margin-top: 10px;
+        }
+        
+        
+        .column {
+          flex: 50%;
+          padding: 20px;
+        }
+    </style>
 </head>
 <body>
     <header>
@@ -19,13 +31,23 @@
     <hr>
     <div class="wrapper">
         <h2>Contact Us:</h2>
-
-        <form action="contactProcess.php" method="POST">
-            Name:<br> <input type="text" name="name"><br>
-            Email:<br> <input type="email" name="email"><br>
-            Content:<br> <textarea name="contact" rows="4" cols="50"></textarea><br>
-            <input type="submit" value="Send Message">
-        </form>
+        <div class="row">
+            <div class="column">
+                <p>Do you have a question for us? Are you looking to receive an estimate?
+                   If so, gives us a call at (951) 123-4567 or fill out the simple form. 
+                   If you choose to fill out the form, we will review your information and 
+                   call you back to discuss your lighting issue and offer to provide an onsite
+                   estimate for the work.</p>
+            </div>
+            <div class="column">
+                <form action="contactProcess.php" method="POST">
+                    Name:<br> <input type="text" name="name"><br>
+                    Email:<br> <input type="email" name="email"><br>
+                    Content:<br> <textarea name="contact" rows="4" cols="50"></textarea><br>
+                    <input type="submit" value="Send Message">
+                </form>
+            </div>
+        </div>
     </div>
     <footer>
         <p id="copyright">Copyright © 2020 The Recessed Light Guy</p>
